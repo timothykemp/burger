7d327eb824f2e1a9cfb509e2aabf32c1bc9dfd6c
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-12 22:53:58 -0600

    added burger image and bulma CSS package with some initial formatting

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon');
 INSERT INTO burgers (burger_name) VALUES ('Mushroom');
 INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
 
